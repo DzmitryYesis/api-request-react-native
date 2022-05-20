94e7bca240202a0b662bd0ec0df2318ec7b0d55c
DzmitryYesis <yesis1988@mail.ru>
2022-05-20 20:32:35 +0300

    feat: add styles

--- src/screens/AllPokemon.tsx
+++ src/screens/AllPokemon.tsx
@@ -1,6 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {api} from '../api/api';
+import {useAppNavigation} from './types';
+
+type Pokemon = {
+    id: number
+    name: string
+    sprites: {
+        other: {
+            'official-artwork': {
+                'front_default': string
+            }
+        }
+    }
+}
+
 
 type PokemonListItem = {
     name: string
@@ -10,7 +24,7 @@ type PokemonListItem = {
 
 export const AllPokemon = () => {
     const [dataPokemon, setDataPokemon] = useState<PokemonListItem[] | null>(null)
-
+    const navigation = useAppNavigation()
     const getPokemon = async () => {
         const result = await api.getAllPokemon(40)
         setDataPokemon(result.data.results)
@@ -20,17 +34,34 @@ export const AllPokemon = () => {
     }, [])
     console.log(dataPokemon)
     return (
-        <View style={{flex: 1}}>
+        <View style={{flex: 1, paddingHorizontal: 20}}>
             <FlatList
                 data={dataPokemon}
                 renderItem={({item}) => {
-                return (
-                    <View>
-                        <Text>{item.name}</Text>
-                    </View>
-                )
-            }}
+                    return (
+                        <TouchableOpacity activeOpacity={0.7}
+                                          onPress={() => navigation.navigate('CurrentPokemon', {url: item.url})}>
+                            <View style={styles.row}>
+                                <Text style={styles.pokemonName}>{item.name}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                }}
                 keyExtractor={(item) => item.name}/>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    row: {
+        height: 48,
+        backgroundColor: '#9e8787',
+        marginVertical: 4,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    pokemonName: {
+        fontWeight: '500',
+        fontSize: 26
+    }
+})
